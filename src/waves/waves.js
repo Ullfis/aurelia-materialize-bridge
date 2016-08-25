5c3fcaa2318516111a3c742f1e918b282d79420d
Eyjolfur Vatne <eyjolfur.vatne@live.no>
2016-08-25 09:51:37 +0200

    chore(waves): test amd module during attach

--- src/waves/waves.js
+++ src/waves/waves.js
@@ -34,7 +34,9 @@ export class MdWaves {
     }
 
     this.attributeManager.addClasses(classes);
-    Waves.attach(this.element);
+    if (!(typeof require === "function" && typeof require.specified === "function" && require.specified('waves'))) {
+      Waves.attach(this.element);
+    }
   }
 
   detached() {
